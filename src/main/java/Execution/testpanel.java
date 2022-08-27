6927db3ff045f545b081040d97c7ba79315c9163
Eikichiiii <oooeikichiooo@gmail.com>
2022-08-27 12:32:44 +0200

    First commit

---
+++ src/main/java/Execution/testpanel.java
@@ -0,0 +1,13 @@
+package Execution;
+import javax.swing.JOptionPane;
+
+public class testpanel {
+
+	public static void main(String[] args) {
+	
+		
+		JOptionPane.showInputDialog("halllllo");
+
+	}
+
+}
